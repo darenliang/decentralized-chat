d0ec76b083e31646772e19031985ea4ae82f228a
Daren Liang <darenliang@outlook.com>
2019-11-29 22:56:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow() {
     // Create the browser window.
@@ -14,10 +14,10 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
 
     // and load the index.html of the app.
-    win.loadFile('layout/index.html')
+    win.loadFile('layout/index.html').then(_ => null);
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -31,7 +31,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -40,7 +40,7 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
@@ -48,7 +48,7 @@ app.on('activate', () => {
     if (win === null) {
         createWindow()
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
